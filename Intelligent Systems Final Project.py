a9ac241575f28b7eb99f3a91d2b722bdaca9d3ad
SalyaW <43365930+SalyaW@users.noreply.github.com>
2020-06-16 16:25:50 +0700

    Add files via upload

---
+++ Intelligent Systems Final Project.py
@@ -0,0 +1,290 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 12:04:49 2020
+
+@author: Salya
+
+References:
+    https://www.youtube.com/watch?v=JTj-WgWLKFM
+    https://towardsdatascience.com/machine-learning-project-predicting-boston-house-prices-with-regression-b4e47493633d
+    https://towardsdatascience.com/predicting-house-prices-with-linear-regression-machine-learning-from-scratch-part-ii-47a0238aeac
+    
+
+"""
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import rc
+import unittest
+
+from sklearn.datasets import load_boston
+
+
+print("----------------------")
+print("House Price Prediction")
+print("----------------------")
+
+boston = load_boston()
+print(boston)
+
+#Transforms raw data into data frames
+
+df_x = pd.DataFrame(boston.data, columns=boston.feature_names) 
+df_y = pd.DataFrame(boston.target)
+
+
+#displays Dataframe
+df_x.describe()
+
+
+#linear Regression
+
+reg = linear_model.LinearRegression()
+
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size= 0.33, random_state=42)
+
+
+#train the model
+reg.fit(x_train, y_train)
+
+#Print coef
+print(reg.coef_) 
+
+#Print Predictions
+y_pred = reg.predict(x_test)
+print(y_pred)
+
+print(y_test)
+
+#check accuracy
+
+print(np.mean((y_pred - y_test)**2))
+
+print (mean_squared_error(y_test,y_pred))
+
+%matplotlib inline
+
+
+# Train Data Set 
+df_train = pd.read_csv('train.csv')
+df_train['SalePrice'].describe()
+# Plots Data for Sales Proces
+sns.distplot(df_train['SalePrice']);
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+
+rcParams['figure.figsize'] = 14, 8
+
+RANDOM_SEED = 20
+
+np.random.seed(RANDOM_SEED)
+
+
+
+def run_tests():
+  unittest.main(argv=[''], verbosity=1, exit=False)
+  
+
+
+# Train Data Set 
+df_train = pd.read_csv('train.csv')
+df_train['SalePrice'].describe()
+# Plots Data for Sales Proces
+sns.distplot(df_train['SalePrice']);
+
+
+
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000), s=32);
+
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(14, 8))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+
+
+k = 9 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+f, ax = plt.subplots(figsize=(14, 10))
+sns.heatmap(df_train[cols].corr(), vmax=.8, square=True);
+
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars']
+sns.pairplot(df_train[cols], size = 4);
+
+#check missing Data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+
+
+
+#Predicting the data
+
+x = df_train['GrLivArea']
+y = df_train['SalePrice']
+
+#print (x)
+
+x = (x - x.mean()) / x.std()
+x = np.c_[np.ones(x.shape[0]), x] 
+
+x.shape
+
+#print(x.shape)
+
+
+#
+def loss(h, y):
+  sq_error = (h - y)**2
+  n = len(y)
+  #print (h)
+  return 1.0 / (2*n) * sq_error.sum()
+
+class TestLoss(unittest.TestCase):
+
+  def test_zero_h_zero_y(self):
+    self.assertAlmostEqual(loss(h=np.array([0]), y=np.array([0])), 0)
+
+  def test_one_h_zero_y(self):
+    self.assertAlmostEqual(loss(h=np.array([1]), y=np.array([0])), 0.5)
+
+  def test_two_h_zero_y(self):
+    self.assertAlmostEqual(loss(h=np.array([2]), y=np.array([0])), 2)
+    
+  def test_zero_h_one_y(self):
+    self.assertAlmostEqual(loss(h=np.array([0]), y=np.array([1])), 0.5)
+    
+  def test_zero_h_two_y(self):
+    self.assertAlmostEqual(loss(h=np.array([0]), y=np.array([2])), 2)
+    
+run_tests()
+
+
+class LinearRegression:
+  
+  def predict(self, X):
+    return np.dot(X, self._W)
+  
+  def _gradient_descent_step(self, X, targets, lr):
+
+    predictions = self.predict(X)
+    
+    error = predictions - targets
+    gradient = np.dot(X.T,  error) / len(X)
+
+    self._W -= lr * gradient
+      
+  def fit(self, X, y, n_iter=100000, lr=0.01):
+
+    self._W = np.zeros(X.shape[1])
+
+    self._cost_history = []
+    self._w_history = [self._W]
+    for i in range(n_iter):
+      
+        prediction = self.predict(X)
+        cost = loss(prediction, y)
+        
+        self._cost_history.append(cost)
+        
+        self._gradient_descent_step(x, y, lr)
+        
+        self._w_history.append(self._W.copy())
+    return self
+
+class TestLinearRegression(unittest.TestCase):
+
+    def test_find_coefficients(self):
+      clf = LinearRegression()
+      clf.fit(x, y, n_iter=2000, lr=0.01)
+      np.testing.assert_array_almost_equal(clf._W, np.array([180921.19555322,  56294.90199925]))
+      
+run_tests()
+
+clf = LinearRegression()
+clf.fit(x, y, n_iter=2000, lr=0.01)
+
+plt.title('Cost Function J')
+plt.xlabel('No. of iterations')
+plt.ylabel('Cost')
+plt.plot(clf._cost_history)
+plt.show()
+
+clf._cost_history[-1]
+
+#Animation
+
+
+#Set the plot up,
+fig = plt.figure()
+ax = plt.axes()
+plt.title('Sale Price vs Living Area')
+plt.xlabel('Living Area in square feet (normalised)')
+plt.ylabel('Sale Price ($)')
+plt.scatter(x[:,1], y)
+line, = ax.plot([], [], lw=2, color='red')
+annotation = ax.text(-1, 700000, '')
+annotation.set_animated(True)
+plt.close()
+
+
+x = df_train[['OverallQual', 'GrLivArea', 'GarageCars']]
+
+x = (x - x.mean()) / x.std()
+x = np.c_[np.ones(x.shape[0]), x] 
+
+clf = LinearRegression()
+clf.fit(x, y, n_iter=2000, lr=0.01)
+
+clf._W
+
+
+plt.title('Cost Function J')
+plt.xlabel('No. of iterations')
+plt.ylabel('Cost')
+plt.plot(clf._cost_history)
+plt.show()
+
+
+
+%matplotlib qt5
+#Generate the animation data,
+def init():
+    line.set_data([], [])
+    annotation.set_text('')
+    return line, annotation
+
+# animation function.  This is called sequentially
+
+def animate(i):
+    x = np.linspace(-5, 20, 1000)
+    y = clf._w_history[i][1]*x + clf._w_history[i][0]
+    line.set_data(x, y)
+    annotation.set_text('Cost = %.2f e10' % (clf._cost_history[i]/10000000000))
+    return line, annotation
+
+animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=300, interval=10, blit=True)
+
+
+
+
+
